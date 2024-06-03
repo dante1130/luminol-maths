0537a14a4f32a82d6935c68d329d34dbf950eb44
dante1130 <andrewdante2@gmail.com>
2024-06-04 00:43:37 +0800

    Add orthogonal basis

---
+++ src/LuminolMaths/VectorUtils.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <LuminolMaths/Vector.hpp>
+
+namespace Luminol::Maths::VectorUtils {
+
+constexpr auto orthogonal_basis(
+    Vector3& vector_a, Vector3& vector_b, Vector3& vector_c
+) -> void {
+    vector_a = vector_a.normalized();
+    vector_c = vector_a.cross(vector_b).normalized();
+    vector_b = vector_c.cross(vector_a);
+}
+
+}  // namespace Luminol::Maths::VectorUtils
